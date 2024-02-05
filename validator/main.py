cfeb4063bc46d5f8a13f81e57181b9989d947dc8
Safeer Mohiuddin <safeer@guardrailsai.com>
2024-02-05 08:21:25 +0000

    uploading saliency_check validator

--- validator/main.py
+++ validator/main.py
@@ -1,9 +1,8 @@
-import re
-import string
-from typing import Any, Callable, Dict, Optional
+import inspect
+import os
+from typing import Any, Callable, Dict, List, Optional, cast
 
-import rstr
-
+from guardrails.utils.openai_utils import get_static_openai_chat_create_func
 from guardrails.validator_base import (
     FailResult,
     PassResult,
@@ -13,59 +12,130 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/regex_match", data_type="string")
-class RegexMatch(Validator):
-    """Validates that a value matches a regular expression.
+@register_validator(name="guardrails/saliency_check", data_type="string")
+class SaliencyCheck(Validator):
+    """Checks that the summary covers the list of topics present in the
+    document.
 
     **Key Properties**
 
-    | Property                      | Description                       |
-    | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `regex_match`                     |
-    | Supported data types          | `string`                          |
-    | Programmatic fix              | Generate a string that matches the regular expression |
+    | Property                      | Description                         |
+    | ----------------------------- | ----------------------------------- |
+    | Name for `format` attribute   | `saliency-check`                    |
+    | Supported data types          | `string`                            |
+    | Programmatic fix              | None                                |
 
     Args:
-        regex: Str regex pattern
-        match_type: Str in {"search", "fullmatch"} for a regex search or full-match option
+
+        docs_dir: Path to the directory containing the documents.
+        threshold: Threshold for overlap between topics in document and summary. Defaults to 0.25
     """  # noqa
 
     def __init__(
         self,
-        regex: str,
-        match_type: Optional[str] = None,
+        docs_dir: str,
+        llm_callable: Optional[Callable] = None,
         on_fail: Optional[Callable] = None,
+        threshold: float = 0.25,
+        **kwargs,
     ):
-        # todo -> something forces this to be passed as kwargs and therefore xml-ized.
-        # match_types = ["fullmatch", "search"]
+        """Initialize the SalienceCheck validator.
 
-        if match_type is None:
-            match_type = "fullmatch"
-        assert match_type in [
-            "fullmatch",
-            "search",
-        ], 'match_type must be in ["fullmatch", "search"]'
+        Args:
+            docs_dir: Path to the directory containing the documents.
+            on_fail: Function to call when validation fails.
+            threshold: Threshold for overlap between topics in document and summary.
+        """
 
-        super().__init__(on_fail=on_fail, match_type=match_type, regex=regex)
-        self._regex = regex
-        self._match_type = match_type
+        super().__init__(
+            on_fail,
+            docs_dir=docs_dir,
+            llm_callable=llm_callable,
+            threshold=threshold,
+            **kwargs,
+        )
+
+        if llm_callable is not None and inspect.iscoroutinefunction(llm_callable):
+            raise ValueError(
+                "SaliencyCheck validator does not support async LLM callables."
+            )
+
+        self.llm_callable = (
+            llm_callable if llm_callable else get_static_openai_chat_create_func()
+        )
+
+        self._threshold = threshold
+
+        # Load documents
+        self._document_store = {}
+        for doc_path in os.listdir(docs_dir):
+            with open(os.path.join(docs_dir, doc_path)) as f:
+                text = f.read()
+            # Precompute topics for each document
+            self._document_store[doc_path] = self._get_topics(text)
+
+    @property
+    def _topics(self) -> List[str]:
+        """Return a list of topics that can be used in the validator."""
+        # Merge topics from all documents
+        topics = set()
+        for doc_topics in self._document_store.values():
+            topics.update(doc_topics)
+        return list(topics)
+
+    def _get_topics(self, text: str, topics: Optional[List[str]] = None) -> List[str]:
+        """Extract topics from a string."""
+
+        from guardrails import Guard
+
+        topics_seed = ""
+        if topics is not None:
+            topics_seed = (
+                "Here's a seed list of topics, select topics from this list"
+                " if they are covered in the doc:\n\n" + ", ".join(topics)
+            )
+
+        spec = f"""
+<rail version="0.1">
+<output>
+    <list name="topics">
+        <string name="topic" description="few words describing the topic in text"/>
+    </list>
+</output>
+
+<prompt>
+Extract a list of topics from the following text:
+
+{text}
+
+{topics_seed}
+
+Return the output as a JSON with a single key "topics" containing a list of topics.
+
+Make sure that topics are relevant to text, and topics are not too specific or general.
+</prompt>
+</rail>
+    """
+
+        guard = Guard.from_rail_string(spec)
+        _, validated_output, *rest = guard(llm_api=self.llm_callable)  # type: ignore
+        validated_output = cast(Dict, validated_output)
+        return validated_output["topics"]
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        p = re.compile(self._regex)
-        """Validates that value matches the provided regular expression."""
-        # Pad matching string on either side for fix
-        # example if we are performing a regex search
-        str_padding = (
-            "" if self._match_type == "fullmatch" else rstr.rstr(string.ascii_lowercase)
-        )
-        self._fix_str = str_padding + rstr.xeger(self._regex) + str_padding
+        topics_in_summary = self._get_topics(value, topics=self._topics)
 
-        if not getattr(p, self._match_type)(value):
+        # Compute overlap between topics in document and summary
+        intersection = set(topics_in_summary).intersection(set(self._topics))
+        overlap = len(intersection) / len(self._topics)
+
+        if overlap < self._threshold:
             return FailResult(
-                error_message=f"Result must match {self._regex}",
-                fix_value=self._fix_str,
+                error_message=(
+                    f"The summary \nSummary: {value}\n does not cover these topics:\n"
+                    f"{set(self._topics).difference(intersection)}"
+                ),
+                fix_value="",
             )
-        return PassResult()
 
-    def to_prompt(self, with_keywords: bool = True) -> str:
-        return "results should match " + self._regex
+        return PassResult()
